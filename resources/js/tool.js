6bef46b779ddafeb8079bda7e051489672194b5e
Mark van den Broek <mvdnbrk@gmail.com>
2019-06-27 21:58:06 +0200

    Import icon components.

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,6 +1,13 @@
+import IconScan from './components/icons/Scan.vue';
+import IconCloseOutline from './components/icons/CloseOutline.vue';
+import IconCheckmarkOutline from './components/icons/CheckmarkOutline.vue';
+
 import Tool from './components/Tool.vue';
 
 Nova.booting((Vue, router) => {
+    Vue.component('icon-scan', IconScan);
+    Vue.component('icon-close-outline', IconCloseOutline);
+    Vue.component('icon-checkmark-outline', IconCheckmarkOutline);
     router.addRoutes([
         {
             name: 'nova-warehouse-scan',
