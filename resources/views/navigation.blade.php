9f2704cb33a50643c5f8d922ba60b900d6f62922
Mark van den Broek <mvdnbrk@gmail.com>
2019-07-05 17:41:46 +0200

    Updates navigation.

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,6 +1,19 @@
-<router-link tag="h3" :to="{name: 'nova-warehouse-scan'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
+<h3 class="flex items-center font-normal text-white mb-6 text-base no-underline">
     <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
         <path fill="var(--sidebar-icon)" d="M2 0C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2V0zM3 0h2v20H3zM6 0h2v20H6zM9 0h5v20H9zM15 0h2v20h-2zM18 0v20c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2z"/>
     </svg>
     <span class="sidebar-label">Scan</span>
-</router-link>
+</h3>
+
+<ul class="list-reset mb-8">
+    <li class="leading-tight mb-4 ml-8 text-sm">
+        <router-link :to="{name: 'locations.add-inventory.index'}" class="text-white text-justify no-underline dim">
+            Add Inventory
+        </router-link>
+    </li>
+    <li class="leading-tight mb-4 ml-8 text-sm">
+        <router-link :to="{name: 'locations.move-inventory.index'}" class="text-white text-justify no-underline dim">
+            Move Inventory
+        </router-link>
+    </li>
+</ul>
