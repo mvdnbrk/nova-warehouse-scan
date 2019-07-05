a76103190368d3d6b064960fd78388f6b95e836c
Mark van den Broek <mvdnbrk@gmail.com>
2019-07-05 17:07:31 +0200

    Adds ChooseLocation component.

---
+++ resources/js/components/ChooseLocation.vue
@@ -0,0 +1,45 @@
+<template>
+    <form @submit.prevent="next" autocomplete="off">
+        <form-belongs-to-field
+            resource-name="inventories"
+            :field="field"
+        >
+        </form-belongs-to-field>
+
+        <div class="bg-30 flex items-center px-8 py-4">
+            <progress-button
+                class="ml-auto"
+                type="submit"
+            >{{ __('Next') }}</progress-button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        field : {
+            attribute: 'location',
+            name: 'Location',
+            resourceName: 'locations',
+            searchable: true,
+        },
+    }),
+
+    methods: {
+        next() {
+            let selectedResourceId = this.createFormData().get('location');
+
+            if (selectedResourceId) {
+                this.$emit('selected', selectedResourceId);
+            }
+        },
+
+        createFormData() {
+            return _.tap(new FormData(), formData => {
+                this.field.fill(formData);
+            });
+        },
+    }
+}
+</script>
--- resources/js/scan-tool.js
+++ resources/js/scan-tool.js
@@ -2,10 +2,12 @@ import IconScan from './components/icons/Scan.vue';
 import IconCloseOutline from './components/icons/CloseOutline.vue';
 import IconCheckmarkOutline from './components/icons/CheckmarkOutline.vue';
 
+import ChooseLocation from './components/ChooseLocation.vue';
 Nova.booting((Vue, router) => {
     Vue.component('icon-scan', IconScan);
     Vue.component('icon-close-outline', IconCloseOutline);
     Vue.component('icon-checkmark-outline', IconCheckmarkOutline);
+    Vue.component('choose-location', ChooseLocation);
 
     router.addRoutes([
     ]);
