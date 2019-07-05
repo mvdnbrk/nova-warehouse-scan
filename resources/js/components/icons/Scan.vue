16d4cc2212d6df850cc25a02b03e5f11895a86dc
Mark van den Broek <mvdnbrk@gmail.com>
2019-07-05 17:29:10 +0200

    Updates icons.

--- resources/js/components/icons/Scan.vue
+++ resources/js/components/icons/Scan.vue
@@ -1,3 +1,5 @@
 <template functional>
-    <path d="M2 0C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2V0zM3 0h2v20H3zM6 0h2v20H6zM9 0h5v20H9zM15 0h2v20h-2zM18 0v20c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2z"/>
+    <path
+        d="M2 0C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2V0zM3 0h2v20H3zM6 0h2v20H6zM9 0h5v20H9zM15 0h2v20h-2zM18 0v20c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2z"
+    />
 </template>
