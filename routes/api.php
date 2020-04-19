daf962dce568d1f2aa3d77d9a88f650275f5cd3b
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-19 20:11:24 +0200

    Update withoutMiddleware

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,14 @@ Route::get('locations/{location}', [LocationController::class, 'show']);
 Route::post('locations/{location}/add-inventory', [AddInventoryController::class, 'store']);
 
 Route::get('locations/{location}/inventory', [InventoryController::class, 'show']);
-Route::post('locations/{location}/move-inventory', [MoveInventoryController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::post('locations/{location}/move-inventory', [MoveInventoryController::class, 'store'])->withoutMiddleware([
+    \App\Http\Middleware\VerifyCsrfToken::class,
+    VerifyCsrfToken::class,
+]);
 
 Route::get('orders/open', [OrderPickListController::class, 'index']);
 Route::get('orders/{order}/picklist', [OrderPickListController::class, 'show']);
-Route::post('orders/{order}/fulfilled', [OrderFulfilledController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::post('orders/{order}/fulfilled', [OrderFulfilledController::class, 'store'])->withoutMiddleware([
+    \App\Http\Middleware\VerifyCsrfToken::class,
+    VerifyCsrfToken::class,
+]);
