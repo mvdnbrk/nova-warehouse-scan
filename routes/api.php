377007b7bba036f624c704f430b3684fefce7eb7
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-19 19:39:03 +0200

    Disable VerifyCsrfToken middleware on routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Just\Warehouse\Nova\Scan\Http\Controllers\AddInventoryController;
 use Just\Warehouse\Nova\Scan\Http\Controllers\InventoryController;
@@ -24,8 +25,8 @@ Route::get('locations/{location}', [LocationController::class, 'show']);
 Route::post('locations/{location}/add-inventory', [AddInventoryController::class, 'store']);
 
 Route::get('locations/{location}/inventory', [InventoryController::class, 'show']);
-Route::post('locations/{location}/move-inventory', [MoveInventoryController::class, 'store']);
+Route::post('locations/{location}/move-inventory', [MoveInventoryController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class]);
 
 Route::get('orders/open', [OrderPickListController::class, 'index']);
 Route::get('orders/{order}/picklist', [OrderPickListController::class, 'show']);
-Route::post('orders/{order}/fulfilled', [OrderFulfilledController::class, 'store']);
+Route::post('orders/{order}/fulfilled', [OrderFulfilledController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class]);
